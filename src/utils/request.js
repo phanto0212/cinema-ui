ecc9ed71dd668f1966af39d760787e91e07b5e15
phanto0212 <phanto02122004@gmail.com>
2025-04-30 17:23:38 +0700

    new server

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: 'http://localhost:8081',
+    baseURL: 'https://cinema-be-1.onrender.com',
     // withCredentials: true
 })
 
